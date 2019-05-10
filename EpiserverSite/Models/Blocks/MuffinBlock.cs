4b5a8b9f048e5dd2fb531f75d6ee119d7d1fb6dd
Olivia <denbuw@gmail.com>
2019-05-10 14:17:14 +0200

    - Added _Layout with specific layout chosen from template - Added a muffinBlock with one property - Added files for frontend in Static Folder

---
+++ EpiserverSite/Models/Blocks/MuffinBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverSite.Models.Blocks
+{
+    [ContentType(DisplayName = "MuffinBlock", GUID = "a79505a1-35ce-44bb-9a5e-3dcc70d34637", Description = "")]
+    public class MuffinBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Name",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString Name { get; set; }
+    }
+}
---
+++ EpiserverSite/Models/Pages/StartPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverSite.Models.Pages
+{
+    [ContentType(DisplayName = "Start",
+                 GUID = "bc4cc5c5-5885-4205-8e4c-1596abae66dd",
+                 Description = "The home page for a website with an area for blocks and partial pages.",
+                 GroupName = SiteGroupNames.Specialized,
+                 Order = 10)]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(Name = "Heading",
+                 Description = "If the Heading is not set, the page falls back to showing the Name.",
+                 GroupName = SystemTabNames.Content, Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
