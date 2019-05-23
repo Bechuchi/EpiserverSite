be5ba43ab2d22aad73307688fa02ed3a88ebb3cd
Olivia <denbuw@gmail.com>
2019-05-23 12:36:24 +0200

    - InfoBlock

---
+++ EpiserverSite/Models/Blocks/InfoBlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverSite.Models.Blocks
+{
+    [ContentType(DisplayName = "InfoBlock", GUID = "019d8aa6-059f-40b9-88d2-873becf8b31e", Description = "")]
+    public class InfoBlock : BlockData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Info block",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string InfoText { get; set; }
+
+
+
+    }
+}
---
+++ EpiserverSite/Models/ViewModels/InfoBlockViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpiserverSite.Models.ViewModels
+{
+    public class InfoBlockViewModel
+    {
+        public string InfoText { get; set; }
+    }
+}
